12f9923b6f9cce8fa060da40049a4af2d7893c71
unless <jbpv@163.com>
2025-11-17 11:16:03 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -77,7 +77,7 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
-UPDATE_PACKAGE "daed" "QiuSimons/luci-app-daed" "main"
+UPDATE_PACKAGE "daed" "QiuSimons/luci-app-daed" "master"
 UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
 
 #更新软件包版本

c6afa9e64fed5d60fe8784ded052d7ba2939a02b
unless <jbpv@163.com>
2025-12-02 14:43:49 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -13,10 +13,9 @@ sed -i \
 -e 's/kmod-ath11k-ahb //g' \
 -e 's/wpad-openssl //g' \
 -e 's/kmod-ath11k-pci //g' \
--e 's|\tkmod-qca-nss-drv-wifi-meshmgr \\||g' \
 ../target/linux/qualcommax/Makefile
 sed -i '/\tDEVICE_PACKAGES := ipq-wifi-zn_m2/d' ../target/linux/qualcommax/image/ipq60xx.mk
-rm -r -f  ../feeds/luci/applications/luci-app-daed
+# rm -r -f  ../feeds/luci/applications/luci-app-daed
 # rm -r -f  ../feeds/packages/net/daed
 #安装和更新软件包
 UPDATE_PACKAGE() {
